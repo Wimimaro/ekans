e6f19c3ec85c06fc7f3df548db565df6cacf73dc
Wimimaro <waylzender@gmail.com>
2024-10-24 15:06:01 +0200

    Interaction fruit serpent et déplacement serpent

--- ekans.js
+++ ekans.js
@@ -4,22 +4,31 @@ var lignes = 20;
 var context;
 var plateau;
 
-var fruitX = 10 * boxsize;
-var fruitY = 10 * boxsize;
+var fruitX;
+var fruitY;
 
 var serpentX = 5 * boxsize;
 var serpentY = 5 * boxsize;
+
+var dx = 0;
+var dy = 0;
+
+var score = 0;
 
 window.onload = function() {
     plateau = document.getElementById("board");
     plateau.height = lignes * boxsize;
     plateau.width = cols * boxsize;
-    context = plateau.getContext("2d");
+    context = plateau.getContext("2d")
 
-    update();
+    generatefruit();
+    document.addEventListener("keyup", deplacement)
+    setInterval(update, 200);
 }
 
 function update() {
+    serpentX += dx;
+    serpentY += dy;
 
     context.fillStyle="black";
     context.fillRect(0, 0, plateau.width, plateau.height);
@@ -29,4 +38,40 @@ function update() {
 
     context.fillStyle="lime";
     context.fillRect(serpentX, serpentY, boxsize, boxsize);
+
+    if(serpentX == fruitX && serpentY == fruitY){
+        gestionscore();
+        context.clearRect(fruitX,fruitY,boxsize,boxsize);
+        generatefruit();
+    }
 }
+
+function generatefruit(){
+    fruitX = Math.floor(Math.random() * lignes) * boxsize;
+    fruitY = Math.floor(Math.random() * cols) * boxsize;
+}
+
+function deplacement(e){
+    if(e.code === "ArrowUp"){
+        dx = 0;
+        dy = -boxsize;
+    }
+    if(e.code == "ArrowDown"){
+        dx = 0;
+        dy = boxsize;
+    }
+    if(e.code == "ArrowLeft"){
+        dx = -boxsize;
+        dy = 0;
+    }
+    if(e.code == "ArrowRight"){
+        dx = boxsize;
+        dy = 0;
+    }
+}
+
+function gestionscore(){
+    score++;
+    document.getElementById("score").textContent = score;
+}
+
